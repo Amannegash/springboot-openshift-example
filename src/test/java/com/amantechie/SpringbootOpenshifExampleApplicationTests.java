6879141b4e7a82c038bb4504b9341610b8ab5824
Amannegash <84345555+Amannegash@users.noreply.github.com>
2022-10-13 14:56:51 -0700

    Add files via upload

---
+++ src/test/java/com/amantechie/SpringbootOpenshifExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amantechie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootOpenshifExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
